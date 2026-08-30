b4697d85e178dd68dc70832af37c741c63ad781e
Ahmed Elawaady <elawaady.official@gmail.com>
2026-08-30 19:53:45 +0300

    Load EXD section primitives

--- header.php
+++ header.php
@@ -18,6 +18,7 @@ if (!isset($page_title)) {
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="storefront.css">
     <link rel="stylesheet" href="exd-media.css">
+    <link rel="stylesheet" href="exd-sections.css">
     <link rel="stylesheet" href="motion.css">
 </head>
 <body>
