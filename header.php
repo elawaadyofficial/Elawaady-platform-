bda467669f16dbea4eadb70340fbecbc7257faab
Ahmed Elawaady <elawaady.official@gmail.com>
2026-08-30 11:54:31 +0300

    Load EXD media slot styles

--- header.php
+++ header.php
@@ -17,6 +17,7 @@ if (!isset($page_title)) {
     <link rel="stylesheet" href="exd-tokens.css">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="storefront.css">
+    <link rel="stylesheet" href="exd-media.css">
     <link rel="stylesheet" href="motion.css">
 </head>
 <body>
